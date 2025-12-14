b364282c724c3ed67bf67db5c8ba3ddf9dfab2bb
Jefferson <jefferson.swanto@gmail.com>
2025-12-14 15:03:27 +0700

    test: config dummy page

---
+++ src/app/color/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex h-full">
+      <div className="text-3xl pl-4">Configuration Page</div>
+      <div></div>
+    </div>
+  );
+}
